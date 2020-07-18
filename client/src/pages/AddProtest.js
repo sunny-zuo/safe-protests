5177625cd180efe3423e176c953cd521ea459203
Carol Xu <carolx715@gmail.com>
2020-07-18 18:51:20 -0400

    layout of add-protest page

--- client/src/pages/AddProtest.js
+++ client/src/pages/AddProtest.js
@@ -3,10 +3,34 @@ import "./css/style.css";
 
 const AddProtest = () => {
 	return (
-		<div className="body">
-			<h1 className="title">
-				Add A New Protest
-			</h1>
+		<div className="body" style={{textAlign: "left"}}>
+			<div className="text">
+				<h1 className="title">
+					Add A New Protest
+				</h1>
+
+
+				<h2 className="subtitle">
+					1. Name your protest: 
+				</h2>
+
+
+				<h2 className="subtitle">
+					2. Date of event: 
+				</h2>
+
+				<h2 className="subtitle">
+					3. Location: 
+				</h2>
+
+				<h2 className="subtitle">
+					4. Add a description!
+				</h2>
+
+				<h2 className="subtitle">
+					X. Add Organizers: 
+				</h2>
+			</div>
 		</div>
 	);
 };
