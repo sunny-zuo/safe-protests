f0e10c9736b3abc8c5c5b72e655e90dbc708f478
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2020-07-18 18:03:48 -0400

    updated our about page

--- client/src/umarPages/aboutSafeProtests.js
+++ client/src/umarPages/aboutSafeProtests.js
@@ -4,8 +4,9 @@ const aboutSafeProtests= () => {
     return (
        <div>
            <h1><b>About Safe Protests</b></h1>
-           <p>Safe Protests is a web app created by Carol, Rahul, Sunny and Umar for Set.Hacks() 2020. This app was inspired by the recent unrest in North America over George Floyd's death and the Black Lives Matter Movement.</p>
-       </div>
+           <p>Safe Protests is a web app created by Carol, Rahul, Sunny and Umar for Set.Hacks() 2020. </p>
+           <p>From the womens' suffrage movement to Martin Luther King Jr.'s rallies, citizens has always relied on protests and public demonstrations to make change happen. With recent events in mind, it is becoming apparent that protesters need a way to protect themselves and stay informed during the middle of a protest or strike. Safe Protests is an app for organizing and staying safe during protests, providing live updates from protest organizers, information about protester rights, marking dangerous locations on a live-updated map, and the ability to notify contacts with a participant's last location if something goes wrong.</p>
+           </div>
     );
 }
  
