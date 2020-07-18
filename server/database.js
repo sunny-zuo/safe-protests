834e24e4cd64bb97415a03370cc0a17655857c11
sunny-zuo <me@sunnyzuo.com>
2020-07-18 15:41:31 -0600

    feat: get_protests endpoint

--- server/database.js
+++ server/database.js
@@ -4,17 +4,32 @@ const MongoClient = require('mongodb').MongoClient;
 const url = `mongodb+srv://admin:${process.env.DB_PASSWORD}@safeprotests.yx0su.mongodb.net/db?retryWrites=true&w=majority`;
 
 // Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoClient = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = null;
+
+mongoClient.connect((err, client) => {
+    if (err) { console.error(err) };
+    db = client.db("database");
+})
 
 async function insertProtest(obj) {
-    client.connect(err => {
-        const collection = client.db("test").collection("protests");
-        collection.insertOne(obj, function(err, res) {
-            if (err) throw err;
-            console.log(`${JSON.stringify(obj)} inserted`);
-            client.close();
+    const collection = db.collection("protests");
+    collection.insertOne(obj, function (err, res) {
+        if (err) throw err;
+        console.log(`${JSON.stringify(obj)} inserted`);
+    });   
+}
+
+async function getProtests(page = 0) {
+    return new Promise((resolve, reject) => {
+        const collection = db.collection("protests");
+        collection.find().skip(page).sort({ _id: -1 }).limit(10).toArray(function (err, res) {
+            if (err) reject(err);
+            resolve(res);
         });
-    })
+    });
 }
 
 exports.insertProtest = insertProtest;
+exports.getProtests = getProtests;
--- server/index.js
+++ server/index.js
@@ -40,5 +40,20 @@ app.post('/add_protest', function(req, res) {
     res.status(resCode).send({success: success, msg: resMsg});
 });
 
+app.get('/get_protests', async (req, res) => {
+    let skip = 0;
+    if (req.query.page) {
+        skip = (req.query.page * 10) - 10 // each page contains 10 protests and thus will skip 10 more per page
+    }
+
+    try {
+        let data = await database.getProtests(skip);
+        res.status(200).send({data: data, success: true});
+    } catch (err) {
+        res.status(503).send({success: false, msg: 'Internal Server Error'});
+    }
+});
+
 app.listen(8000);
+
 console.log("Server listening on port 8000");
