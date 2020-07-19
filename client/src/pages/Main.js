d8d67fff9f41a9bababe161877d684f2b2548b93
Carol Xu <carolx715@gmail.com>
2020-07-18 20:50:49 -0400

    more main updates

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./css/Main.css";
 import safeprotest from "./img/safe-protests.png";
+import or from "./img/or.png";
 
 const Main = () => {
 	return (
 		<div className="body">
 			<div className="title-container">
-				<img src={safeprotest}></img>
+				<img class=".img" src={safeprotest}></img>
 			</div>
 			<p className="text">
 				Keeping people safe during protests with live updates and info
 			</p>
+			<a className="btn" href="#">SIGN IN</a>
+			<div className="or-container">
+				<img class=".img" src={or}></img>
+			</div>
+			<a className="btn" href="#">REGISTER</a>
 		</div>
 	);
 };
