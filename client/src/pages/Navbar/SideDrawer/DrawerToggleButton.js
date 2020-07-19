853c2265c277c754c1cf38a127c439fdee69db05
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-07-18 21:22:06 -0400

    fixed image

--- client/src/pages/Navbar/SideDrawer/DrawerToggleButton.js
+++ client/src/pages/Navbar/SideDrawer/DrawerToggleButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../css/DrawerToggleButton.css";
-import buttonIcon from "../../img/menu-icon.jpg";
+import buttonIcon from "../../img/menu-icon.png";
 
 const DrawerToggleButton = (props) => (
 	<button className="toggle_button" onClick={props.click}>
 		{" "}
 		{/* once click the sidedrawer will open and App.js state will be set to true */}
-		<img className="icon" src={buttonicon} />
+		<img className="icon" src={buttonIcon} />
 	</button>
 );
 
