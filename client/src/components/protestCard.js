195a4f99a804ad4b46964b1d1dde53a4daac9644
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-07-19 06:55:08 -0400

    edits

--- client/src/components/protestCard.js
+++ client/src/components/protestCard.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./../pages/css/ProtestCard.css"
+import "./../pages/css/ProtestCard.css";
 
 export default function ProtestCard(props) {
-    return (
-        <Link to={{ pathname: "/view-protest", search: `?protestID=${props._id}`}}>
-            <div className='protestCard'>
-                <h1 className="cardtitle">{props.name}</h1>
+	return (
+		<Link to={{ pathname: "/view-protest", search: `?protestID=${props._id}` }}>
+			<div className="protestCard">
+				<div className="header-container">
+					<h1 className="cardtitle">{props.name}</h1>
+				</div>
 
-                <div className="dateloc-container">
-                    <h2>Date: {props.time}</h2>
-                    <h2>Location: {props.location}</h2>
-                </div>
+				<div className="dateloc-container">
+					<h2>Date: {props.time}</h2>
+					<h2>Location: {props.location}</h2>
+				</div>
 
-                <div class="card-subinfo">
-                    <h4>{props.description}</h4>
-                    <h3>Organizers: {props.organizer}</h3>
-                    <h3>Signed up: {props.protestorCount}</h3>
-                    <h3>Status: {props.status}</h3>
-                </div>
-            </div>
-        </Link>
-    )
+				<div class="card-subinfo">
+					<h4>{props.description}</h4>
+					<h3 className="edit">Organizers: {props.organizer}</h3>
+					<h3 className="special">Signed up: {props.protestorCount}</h3>
+					<h3 className="edit">Status: {props.status}</h3>
+				</div>
+			</div>
+		</Link>
+	);
 }
