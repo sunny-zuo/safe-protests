16cfdb33b4ae6ee5bf088549f93e9a2b47156d6c
Carol Xu <carolx715@gmail.com>
2020-07-18 23:05:13 -0400

    styled BrowseProtests.js

--- client/src/pages/BrowseProtests.js
+++ client/src/pages/BrowseProtests.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./css/Newsfeed.css";
 
 const BrowseProtests = () => {
 	return (
-		<div>
+		<div className="newsfeed-body">
 			<h1 className="title">This is the Browse Protests page</h1>
+			<div className="placeholder-postbox"></div>
+			<div className="placeholder-postbox"></div>
+			<div className="placeholder-postbox"></div>
 		</div>
 	);
 };
