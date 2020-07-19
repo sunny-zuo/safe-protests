ed1ec2181a0d4f358a5ce0ee685e0b992da6f8f1
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-07-19 06:28:15 -0400

    edits

--- client/src/pages/BrowseProtests.js
+++ client/src/pages/BrowseProtests.js
@@ -6,28 +6,28 @@ class BrowseProtests extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			protestData: ""
-		}
+			protestData: "",
+		};
 	}
 
 	componentDidMount() {
-		fetch('http://localhost:8000/get_protests?page=1')
-			.then(response => response.json())
-			.then(data => this.setState({ protestData: data.data }));
+		fetch("http://localhost:8000/get_protests?page=1")
+			.then((response) => response.json())
+			.then((data) => this.setState({ protestData: data.data }));
 	}
 
 	render() {
 		if (this.state.protestData === "") {
 			return null;
 		}
-		
-		const Cards = this.state.protestData.map(protest => <ProtestCard {...protest} /> )
+
+		const Cards = this.state.protestData.map((protest) => (
+			<ProtestCard {...protest} />
+		));
 		return (
 			<div className="newsfeed-body">
 				<h1 className="title">Latest Updates on your Protests</h1>
-				<div className="protestCards">
-					{Cards}
-				</div>
+				<div className="protestCards">{Cards}</div>
 			</div>
 		);
 	}
