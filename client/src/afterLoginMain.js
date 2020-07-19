5a72ab0d0b03df950e115d845a70a0507ad9fdbb
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2020-07-18 21:15:39 -0400

    add after login feed page

--- client/src/afterLoginMain.js
+++ client/src/afterLoginMain.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./css/Main.css";
 import safeprotest from "./img/safe-protests.png";
+import database from "database"
 
 const afterLoginMain = () => {
-	return (
+    const post = new database();
+    var data = post.getProtests()
+    data.forEach((data) => {
+        // Log each movie's title
+        console.log(data.title)
+      })
+    return (
 		<div className="body">
 			<div className="title-container">
 				<img src={safeprotest}></img>
