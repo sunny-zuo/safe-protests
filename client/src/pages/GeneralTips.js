f02b69a85d8051e850f20feb5bed41fc6e505f37
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-07-18 20:24:10 -0400

    cleaned up pages

--- client/src/pages/GeneralTips.js
+++ client/src/pages/GeneralTips.js
@@ -1,9 +1,137 @@
 import React from "react";
+import home from "./home";
 
 const GeneralTips = () => {
 	return (
 		<div>
-			<h1>This is the General Tips page</h1>
+			<h1>
+				<b>Ensuring Your Safety During a Protest</b>
+			</h1>
+			<p>
+				Here are some general tips on how to protest safetly courtesy of Amnesty
+				International.
+			</p>
+			<h2>
+				<b>What To Do:</b>
+			</h2>
+			<ul>
+				<li>
+					{" "}
+					Plan ahead: For essential needs, careand supplies. Know what to
+					expect.Know how to get assistance. Plan forhow to re-contact your
+					buddies if separated
+				</li>
+				<li>
+					Be calm and focused: when things get intense, react to danger and
+					warning signs sooner not later
+				</li>
+				<li>
+					Watch for signs of physical and mental problems in yourself and
+					others. Coold down others who exhibit panic behaviour
+				</li>
+				<li>
+					Document: film or write down police actions, brutality and injuries
+				</li>
+			</ul>
+			<h2>
+				<b>What Not To Do</b>
+			</h2>
+			<ul>
+				<li>
+					Don't put vaseline, mineral oil, oil-based sunscreen or moisturizers
+					on skin as they can trap chemicals
+				</li>
+				<li>
+					Don't wear contact lenses, which can trap irritating chemicals
+					underneath
+				</li>
+				<li>
+					Don't wear things which can easily be grabbed (i.e. jewelry, ties,
+					loose hair)
+				</li>
+				<li>
+					Don't go alone, if you can help it - go with an affinity group or some
+					friends who know you well
+				</li>
+				<li>Don't forget to eat food and drink lots of water</li>
+			</ul>
+			<h2>
+				<b>What To Bring</b>
+			</h2>
+			<ul>
+				<li>
+					Water in a plastic water bottle with squirt top, to drink and to wash
+					off your skin or eyes
+				</li>
+				<li>Energy Snacks</li>
+				<li>Identification and emergency contact information</li>
+				<li>Enough money for a pay-phone, food and transportation</li>
+				<li>Watch, paper and pens for accurate documentation of events</li>
+				<li>
+					Inhaler, epipen, insulin and several days of prescription medication
+				</li>
+				<li>
+					Menstrual pads. Avoid using tampons as if you're arrested you may not
+					have a chance to change
+				</li>
+				<li>Basic first aid kit</li>
+				<li>Wet wipes and tissue</li>
+			</ul>
+			<h2>
+				<b>Dealing With Teargas</b>
+			</h2>
+			<ul>
+				<li>
+					Avoid use of oils and lotion because they can trap the chemicals and
+					thereby prolong exposure
+				</li>
+				<li>
+					Gas masks provide the best facial protection, if properly fitted and
+					sealed. Alternatively, googles, respirators, or a wet bandana over the
+					nose and mouth will help.
+				</li>
+				<li>
+					STAY CALM. Panicking increases the irritation. Breathe slowly and
+					remember it is only temporary.
+				</li>
+				<li>
+					Blow your nose, rinse your mouth, cough and spit. Try not to swallow.
+				</li>
+				<li>
+					Wearing contcts: you must remove the lenses or get someone to remove
+					them for you with CLEAN, uncontaminated fingers. Destroy the lenses
+					after the exposure.
+				</li>
+				<li>DO NOT RUB IT IN</li>
+				<li>
+					Use an eye flush using a solution of half liquid antacid and half
+					water. This only applies to aluminium hydroxide or magnesium hydroxide{" "}
+				</li>
+			</ul>
+			<h2>
+				<b>What To Wear</b>
+			</h2>
+			<ul>
+				<li>Shatter resistant Swimming Goggles and a N95 Facemask</li>
+				<li>Comfortable, protective shoes that you can run in</li>
+				<li>
+					Clothing covering all your skin to protect from sun and pepper spray
+					exposure
+				</li>
+				<li>
+					Shatter-resistant eye protection (i.e.sunglasses, swim goggles, or gas
+					mask)
+				</li>
+				<li>
+					Bandana to cover nose and mouth soaked in water, lemon juice or
+					vinegar, it can aid in breathing during chemical exposure
+				</li>
+				<li>
+					Fresh clothes in a plastic bag (in case yours get contaminated by
+					chemical weapons)
+				</li>
+				<li>A hat to protect you from the sun and from chemical weapons</li>
+			</ul>
 		</div>
 	);
 };
